89a331b3543b5bab6e6636464b4b077061d84ab5
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-11-08 17:28:28 +0500

    verify token route add

--- index.js
+++ index.js
@@ -13,12 +13,17 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://real-state-listings-beta.vercel.app",
-    ],
+  // cors({
+  //   origin: [
+  //     "http://localhost:3000",
+  //     "https://real-state-listings-beta.vercel.app",
+  //   ],
 
+  //   credentials: true,
+  // })
+
+  cors({
+    origin: ["http://localhost:3000", "https://real-state-listings-beta.vercel.app"],
     credentials: true,
   })
 );
