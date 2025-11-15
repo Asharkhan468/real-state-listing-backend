c200df48a7cdf6e717966070807bf04376456c85
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-11-15 14:20:34 +0500

    TOKEN SEND TO FRONTEND

--- routes/Login.js
+++ routes/Login.js
@@ -28,11 +28,9 @@ route.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
-      path: "/",
-      maxAge: 24 * 60 * 60 * 1000,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-
 
     return res.status(200).json({
       message: "Login successful!",
