ef873e906c1eddb07b886cbd412e5083681ce781
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-11-17 21:05:46 +0500

    SOME CHANGES ADD

--- routes/Login.js
+++ routes/Login.js
@@ -25,20 +25,23 @@ route.post("/login", async (req, res) => {
       { expiresIn: "1h" }
     );
 
+  
     // res.cookie("token", token, {
     //   httpOnly: true,
     //   secure: true,
     //   sameSite: "none",
+    //   path: "/",
     //   maxAge: 7 * 24 * 60 * 60 * 1000,
     // });
 
     res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      path: "/",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  path: "/",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
+
 
     return res.status(200).json({
       message: "Login successful!",
